13d94b21a5313b959031175a3b503e832ce63cc2
Loic L <l.laborderie-boulou@tbs-education.org>
2023-02-01 14:52:31 +0100

    :sparkles: added data to producer and actor

--- src/DataFixtures/PersonneFixtures.php
+++ src/DataFixtures/PersonneFixtures.php
@@ -2,8 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Acteur;
+use App\Entity\Realisateur;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class PersonneFixtures extends Fixture
 {
@@ -11,6 +14,23 @@ class PersonneFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 50 ;$i++){
+            $acteur = new Acteur();
+            $acteur->setPrenom($faker->firstName);
+            $acteur->setNom($faker->lastName);
+            $manager->persist($acteur);
+
+        }
+        
+        for ($i = 0; $i < 5 ;$i++){
+            $realisateur = new Realisateur();
+            $realisateur->setPrenom($faker->firstName);
+            $realisateur->setNom($faker->lastName);
+            $manager->persist($realisateur);
+
+        }
 
         $manager->flush();
     }
